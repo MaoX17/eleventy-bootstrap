85769c535497e1e71a7e7acaeb3685abe8ab1e05
Maurizio Proietti <maurizio.proietti@gmail.com>
2025-03-06 09:51:17 +0100

    cambio_conf

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,18 @@
 //EMS
 
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 	eleventyConfig
 	 	.addPassthroughCopy({
  			"./public/": "/"
-	 	})
-};
-
-
-
-import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
-
-export default function (eleventyConfig) {
+	 	});
+	
 	eleventyConfig.addPlugin(syntaxHighlight);
+
 };
+
+
+
 
