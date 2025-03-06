3af51c85d4c25523b32cdfa615005bdbe2e67360
Maurizio Proietti <maurizio.proietti@gmail.com>
2025-03-06 09:46:03 +0100

    add_eleventy-plugin-syntaxhighlight

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
+//EMS
+
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 	eleventyConfig
 	 	.addPassthroughCopy({
  			"./public/": "/"
 	 	})
+};
+
+
+
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPlugin(syntaxHighlight);
 };
 
