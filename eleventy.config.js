1a7f65fbf705b51f23fb2c829d31b3597ce49208
Maurizio Proietti <maurizio.proietti@gmail.com>
2025-03-13 01:08:54 +0100

    Aggiornamento

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@
 
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
-import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default async function(eleventyConfig) {
 	// Configure Eleventy
@@ -13,17 +12,6 @@ export default async function(eleventyConfig) {
 	
 	eleventyConfig.addPlugin(syntaxHighlight);
 
-	eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
-		// The base URL: defaults to Path Prefix
-		//baseHref: eleventyConfig.pathPrefix,
-
-		// But you could use a full URL here too:
-		baseHref: "https://www.gremapro.it/"
-
-		// Comma separated list of output file extensions to apply
-		// our transform to. Use `false` to opt-out of the transform.
-		//extensions: "html",
-	});
 
 };
 
